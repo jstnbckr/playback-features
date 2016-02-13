9e6c01facd3cf7d103457d9b60e9e8f04c2e5be6
Tom Leach <tom@gc.io>
2016-02-13 11:08:16 -0500

    Created a reusable SynchronizedEventBus.

---
+++ src/main/java/com/netflix/common/SynchronizedEventBus.java
@@ -0,0 +1,63 @@
+package com.netflix.common;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A synchronized (thread-safe) event bus.
+ * 
+ * Allows subscribers to be registered and then notified whenever a new event is
+ * posted to the bus. 
+ * @author tom
+ *
+ * @param <T> the type of the events this bus will accept and propagate to subscribers
+ */
+public class SynchronizedEventBus<T> {
+  
+  /**
+   * Interface which must be implemented by all event subscribers
+   * which are to received event notifications. 
+   * @author tom
+   *
+   * @param <T> the type of the event the wrapping {@link SynchronizedEventBus}
+   *            will pass to the subscriber.
+   */
+  public interface EventSubscriber<T> {
+    /**
+     * Notifies the underlying implementation that the given
+     * event occurred.
+     * @param event the event
+     */
+    public void onEvent(T event);
+  }
+  
+  private final Set<EventSubscriber<T>> subscribers = Collections.synchronizedSet(new HashSet<EventSubscriber<T>>());
+  
+  /**
+   * Registers an {@link EventSubscriber} instance to receive notifications from
+   * this {@link SynchronizedEventBus}. 
+   * 
+   * Note that registering the same subscriber instance more than once has no
+   * effect.
+   * 
+   * @param subscriber the subscriber instance
+   */
+  public void addSubscriber(EventSubscriber<T> subscriber) {
+    this.subscribers.add(subscriber);
+  }
+  
+  /**
+   * Posts a new event to the event bus which in turn will propagate
+   * it to all registered subscribers.
+   * @param event the event
+   */
+  public void postEvent(T event) {
+    // Ensure we synchronize on the Set as we are iterating.
+    synchronized (subscribers) {
+      for (EventSubscriber<T> subscriber : subscribers) {
+        subscriber.onEvent(event);
+      }      
+    }
+  }
+}
---
+++ src/test/java/com/netflix/common/SynchronizedEventBusTest.java
@@ -0,0 +1,56 @@
+package com.netflix.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SynchronizedEventBusTest {
+
+  private class FakeEvent {}
+  
+  private class FakeSubscriber implements SynchronizedEventBus.EventSubscriber<FakeEvent> {
+    private int callCount = 0;
+    
+    @Override
+    public void onEvent(FakeEvent event) {
+      this.callCount++;
+    }
+  }
+  
+  private SynchronizedEventBus<FakeEvent> bus;
+  
+  @Before
+  public void setUp() throws Exception {
+    this.bus = new SynchronizedEventBus<SynchronizedEventBusTest.FakeEvent>();
+  }
+
+  @Test
+  public void testNoSubscribers() {
+    // Really just testing that this doesn't blow up
+    bus.postEvent(new FakeEvent());
+  }
+
+  @Test
+  public void testMultipleSubscribers() {
+    FakeSubscriber subA = new FakeSubscriber();
+    bus.addSubscriber(subA);
+    bus.postEvent(new FakeEvent());
+    FakeSubscriber subB = new FakeSubscriber();
+    bus.addSubscriber(subB);
+    bus.postEvent(new FakeEvent());
+    assertEquals(2, subA.callCount);
+    assertEquals(1, subB.callCount);
+  }
+  
+  public void testAddSubscriberMultipleTimes() {
+    FakeSubscriber subA = new FakeSubscriber();
+    bus.addSubscriber(subA);
+    bus.postEvent(new FakeEvent());    
+    bus.addSubscriber(subA);
+    bus.addSubscriber(subA);
+    bus.postEvent(new FakeEvent());    
+    assertEquals(2, subA.callCount);
+  }
+}
